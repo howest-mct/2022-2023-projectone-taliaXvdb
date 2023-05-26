427ef78edc193b8ba47e723d9d9e60f22a8b21bd
taliaXvdb <talia.van.den.bossche@student.howest.be>
2023-05-26 15:15:36 +0200

    effe pauze

--- backend/repositories/DataRepository.py
+++ backend/repositories/DataRepository.py
@@ -10,25 +10,33 @@ class DataRepository:
             gegevens = request.form.to_dict()
         return gegevens
 
+    #CREATE
     @staticmethod
-    def read_status_lampen():
-        sql = "SELECT * from lampen"
+    def create_user(name, goal, streak):
+        sql = "INSERT INTO user (name, goal, streak) VALUES (%s,%s,%s)"
+        params = [name, goal, streak]
+        return Database.execute_sql(sql, params)
+    
+    def create_reminder(iduser, type, time, amount, fasterWhenHot):
+        sql = "INSERT INTO reminder (iduser, type, time, amount, fasterWhenHot) VALUES (%s,%s,%s,%s,%s)"
+        params = [iduser, type, time, amount, fasterWhenHot]
+        return Database.execute_sql(sql, params)
+    
+    #READ
+    def read_all_users():
+        sql = "SELECT * FROM user"
         return Database.get_rows(sql)
-
-    @staticmethod
-    def read_status_lamp_by_id(id):
-        sql = "SELECT * from lampen WHERE id = %s"
+    
+    def read_user(id):
+        sql = "SELECT * FROM user WHERE userID = %s"
         params = [id]
         return Database.get_one_row(sql, params)
-
-    @staticmethod
-    def update_status_lamp(id, status):
-        sql = "UPDATE lampen SET status = %s WHERE id = %s"
-        params = [status, id]
-        return Database.execute_sql(sql, params)
-
-    @staticmethod
-    def update_status_alle_lampen(status):
-        sql = "UPDATE lampen SET status = %s"
-        params = [status]
-        return Database.execute_sql(sql, params)
+    
+    def read_history_by_userid(id):
+        sql = "SELECT * FROM history WHERE userID = %s"
+        params = [id]
+        return Database.get_rows(sql, params)
+    
+    def read_history():
+        sql = "SELECT * FROM history"
+        return Database.get_rows(sql)
